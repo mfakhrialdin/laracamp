cc02a5c4b1a964c8fa9c33779706e0d73d2f9a9d
M Fakhri Aldin <mfakhri.aldin@gmai.com>
2021-12-06 09:02:57 +0700

    middleware dashboard & role for user and admin, add set to paid button for admin

--- resources/views/emails/checkout/afterCheckout.blade.php
+++ resources/views/emails/checkout/afterCheckout.blade.php
@@ -5,7 +5,7 @@ Hi, {{$checkout->User->name}}
 <br>
 Thank you for register on <b>{{$checkout->Camp->title}}</b>, please see payment instruction by click button below
 
-@component('mail::button', ['url' => route('dashboard')]
+@component('mail::button', ['url' => route('dashboard')])
 Get Invoice
 @endcomponent
 
